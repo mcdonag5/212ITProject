41f30011826b94cb5bb7ea2dc659cc811dfa750f
mcdonag5 <44897074+mcdonag5@users.noreply.github.com>
2020-04-06 10:41:33 +0000

    Made the .net verison of the contact manager

---
+++ ContactManagerDotNetCore/HarrisContactWeb/HarrisContactWeb/Models/PersonalContact.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HarrisContactWeb.Models
+{
+    public class PersonalContact
+    {
+        public int PersonalContactID { get; set; }
+
+        public string ContactFname { get; set; }
+        public string ContactLname { get; set; }
+        public string ContactTel { get; set; }
+        public string ContactEmail { get; set; }
+        public string ContactAddr1 { get; set; }
+        public string ContactAddr2 { get; set; }
+        public string ContactCity { get; set; }
+        public string ContactPostcode { get; set; }
+        public string PersonalTel { get; set; }
+    }
+}
